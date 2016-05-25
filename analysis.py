5fb23b48ccc1b7d0e1e33fa83ca257f739ca3405
Roger Fan <rogerfan.rf@gmail.com>
2016-05-24 23:51:11 -0400

    Finalizing analysis.

--- analysis.py
+++ analysis.py
@@ -60,7 +60,6 @@ def gmm(data, init_mu, init_sigma, init_pi, max_iter=100, diff_tol=1e-3):
     return (curr_mu, curr_sigma, curr_pi), probs, logliks
 
 
-
 def gmm_cat(data_raw, cat_raw, init_mu, init_sigma, init_pi=None,
         max_iter=100, diff_tol=1e-3):
     num_cat = len(np.unique(cat_raw))
@@ -119,6 +118,7 @@ def gmm_cat(data_raw, cat_raw, init_mu, init_sigma, init_pi=None,
     return (curr_mu, curr_sigma, curr_pi), probs, logliks
 
 
+# Handle data
 sdata = pd.read_pickle('./data/data_week.p')
 sd = np.load('./data/data_week_c.npy')
 hour = np.load('./data/data_week_hour.npy')
@@ -130,27 +130,22 @@ hour_cat[np.logical_and(16 <= hour, hour < 18)] = 3 # Evening rush
 hour_cat[np.logical_and(18 <= hour, hour < 22)] = 4 # Evening
 hour_cat[np.logical_or( 22 <= hour, hour <  2)] = 5 # Night
 
-
-fig = plt.figure()
+# Raw data plots
+fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(1, 1, 1)
 morning_rush = np.array(np.logical_and(7 <= hour, hour < 9))
-ax.scatter(sd[morning_rush, 0], sd[morning_rush, 1], alpha=0.08, lw=0.4, s=10)
+ax.scatter(sd[morning_rush, 0], sd[morning_rush, 1], alpha=0.08, lw=0.1, s=10)
 ax.set_xlim(-74.05, -73.75)
 ax.set_ylim(40.55, 40.9)
+fig.savefig('./include/diff_morningrush.png', bbox_inches='tight', dpi=400)
 
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-evening_rush = np.array(np.logical_and(16 <= hour, hour < 18))
-ax.scatter(sd[evening_rush, 0], sd[evening_rush, 1], alpha=0.08, lw=0.4, s=10)
-ax.set_xlim(-74.05, -73.75)
-ax.set_ylim(40.55, 40.9)
-
-fig = plt.figure()
+fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(1, 1, 1)
 night = np.array(np.logical_and(22 <= hour, hour < 24))
-ax.scatter(sd[night, 0], sd[night, 1], alpha=0.08, lw=0.4, s=10)
+ax.scatter(sd[night, 0], sd[night, 1], alpha=0.08, lw=0.1, s=10)
 ax.set_xlim(-74.05, -73.75)
 ax.set_ylim(40.55, 40.9)
+fig.savefig('./include/diff_night.png', bbox_inches='tight', dpi=400)
 
 plt.plot(np.unique(hour, return_counts=True)[0]+0.5,
          np.unique(hour, return_counts=True)[1], color='black')
@@ -158,56 +153,28 @@ for x in [2, 7, 9, 16, 18, 22]:
     plt.axvline(x=x)
 
 
-# BIC
-np.random.seed(2634)
-inds = np.random.choice(sd.shape[0], 200000, replace=False)
-sd_subset = sd[inds]
-hour_cat_subset = hour_cat[inds]
+# # BIC
+# np.random.seed(2634)
+# inds = np.random.choice(sd.shape[0], 200000, replace=False)
+# sd_subset = sd[inds]
+# hour_cat_subset = hour_cat[inds]
 
-np.random.seed(257456)
-bic = np.array([20, 30, 35, 40, 45, 50, 55, 60])
-bic = np.column_stack((bic, np.zeros(len(bic))))
-for i, k in enumerate(bic[:,0]):
-    print(k)
-    gmm_mod = GMM(n_components=int(k), covariance_type='full', min_covar=1e-8)
-    start = time()
-    gmm_mod.fit(sd_subset)
-    print(time() - start)
+# np.random.seed(257456)
+# bic = np.array([5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+# bic = np.column_stack((bic, np.zeros(len(bic))))
+# for i, k in enumerate(bic[:,0]):
+#     print(k)
+#     gmm_mod = GMM(n_components=int(k), covariance_type='full', min_covar=1e-8)
+#     start = time()
+#     gmm_mod.fit(sd_subset)
+#     print(time() - start)
 
-    bic[i,1] = gmm_mod.bic(sd_subset)
-
-np.savetxt('./data/bic.txt', bic)
-k = 40
-
-
-# K-Means
-km = KMeans(k, n_jobs=-2, random_state=234623)
-start = time()
-km.fit(sd)
-print(time() - start)
-
-with open('./km40.p', 'wb') as f_:
-    pickle.dump(km, f_)
-
-# pred = km.predict(sd)
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# for g, c in zip(range(k), cm.Paired(np.linspace(0, 1, k))):
-#     ax.scatter(sd[pred==g, 0], sd[pred==g, 1], c=c)
+#     bic[i,1] = gmm_mod.bic(sd_subset)
+# np.savetxt('./data/bic.txt', bic)
 
 
 # Run GMM
-# init_sigma = [np.cov(sd, rowvar=0) for i in range(k)]
-# init_pi = np.array([1 for i in range(k)])/k
-# init_mu = km.cluster_centers_
-
-# np.random.seed(2046)
-# start = time()
-# res = gmm(sd, init_mu, init_sigma, init_pi, diff_tol=1e-2, max_iter=1000)
-# print(time() - start)
-
-# with open('./res40.p', 'wb') as f_:
-#     pickle.dump(res, f_)
+k = 30
 
 np.random.seed(2046)
 gmm_mod = GMM(n_components=k, covariance_type='full', min_covar=1e-8)
@@ -215,7 +182,7 @@ start = time()
 gmm_mod.fit(sd)
 print(time() - start)
 
-with open('./gmm_mod40.p', 'wb') as f_:
+with open('./gmm_mod30.p', 'wb') as f_:
     pickle.dump(gmm_mod, f_)
 
 
@@ -227,23 +194,85 @@ init_pi = np.tile(gmm_mod.weights_, (6, 1))
 np.random.seed(2046)
 start = time()
 res_cat = gmm_cat(sd, hour_cat, init_mu, init_sigma, init_pi,
-                  diff_tol=1e-2, max_iter=2000)
+                  diff_tol=1e-2, max_iter=1500)
 print(time() - start)
 
-with open('./res_cat40.p', 'wb') as f_:
+with open('./res_cat30.p', 'wb') as f_:
     pickle.dump(res_cat, f_)
 
 
-fig = plt.figure()
+# Plots
+big = [3, 15, 16]
+rest = [i for i in range(30) if i not in big]
+
+fig = plt.figure(figsize=(10, 9))
 ax = fig.add_subplot(1, 1, 1)
-ax.scatter(sd[:,0], sd[:,1])
-ax.scatter(res_cat[0][0][:,0], res_cat[0][0][:,1], color='red')
+ax.scatter(sd[:,0], sd[:,1], alpha=0.08, lw=0.1, s=10, color='0.8')
+ax.scatter(gmm_mod.means_[rest,0], gmm_mod.means_[rest,1],
+           color='black', s=25, lw=1)
+
+x = np.linspace(-74.05, -73.75)
+y = np.linspace(40.55, 40.9)
+X, Y = np.meshgrid(x, y)
+XX = np.array([X.ravel(), Y.ravel()]).T
+Z = gmm_mod.score_samples(XX)[0]
+Z = Z.reshape(X.shape)
+ax.contour(X, Y, Z)
+
+ax.scatter(gmm_mod.means_[big,0], gmm_mod.means_[big,1],
+           color='red', s=25, lw=1)
+ax.set_xlim(-74.05, -73.75)
+ax.set_ylim(40.55, 40.9)
+fig.savefig('./include/gmm_res.png', bbox_inches='tight', dpi=400)
 
 
+inter = [0, 1, 3, 16, 26]
+airport = [2, 4]
+outer = [7, 13, 21]
+interest = inter + airport + outer
+rest = [i for i in range(30) if i not in interest]
 
-
-fig = plt.figure()
+fig = plt.figure(figsize=(8, 7.2))
 ax = fig.add_subplot(1, 1, 1)
-ax.scatter(sd[:,0], sd[:,1])
-ax.scatter(gmm_mod.means_[:,0], gmm_mod.means_[:,1], color='red')
+ax.scatter(sd[:,0], sd[:,1], alpha=0.08, lw=0.1, s=10, color='0.8')
+ax.scatter(res_cat[0][0][rest,0], res_cat[0][0][rest,1],
+            color='black', s=25, lw=1)
+ax.scatter(res_cat[0][0][interest,0], res_cat[0][0][interest,1],
+            color='blue', s=25, lw=1)
+for i, j in enumerate(interest):
+    ax.annotate(str(i), xy=res_cat[0][0][j], color='blue',
+                xytext=res_cat[0][0][j]+np.array([0.002, 0.002]))
+ax.set_xlim(-74.05, -73.75)
+ax.set_ylim(40.55, 40.9)
+fig.savefig('./include/gmm_cat_res.png', bbox_inches='tight', dpi=400)
 
+
+gmm_morn = GMM(n_components=k, covariance_type='full', min_covar=1e-8)
+gmm_morn.means_ = res_cat[0][0]
+gmm_morn.covars_ = res_cat[0][1]
+gmm_morn.weights_ = res_cat[0][2][1]
+Z_morn = gmm_morn.score_samples(XX)[0]
+Z_morn = Z_morn.reshape(X.shape)
+
+gmm_night = GMM(n_components=k, covariance_type='full', min_covar=1e-8)
+gmm_night.means_ = res_cat[0][0]
+gmm_night.covars_ = res_cat[0][1]
+gmm_night.weights_ = res_cat[0][2][5]
+Z_night = gmm_night.score_samples(XX)[0]
+Z_night = Z_night.reshape(X.shape)
+
+fig_morn = plt.figure(figsize=(6, 5.4))
+ax = fig_morn.add_subplot(1, 1, 1)
+ax.scatter(sd[:,0], sd[:,1], alpha=0.08, lw=0.1, s=10, color='0.8')
+cs = ax.contour(X, Y, Z_morn)
+ax.set_xlim(-74.05, -73.75)
+ax.set_ylim(40.55, 40.9)
+fig_morn.savefig('./include/gmm_cat_morn.png', bbox_inches='tight', dpi=400)
+
+fig_night = plt.figure(figsize=(6, 5.4))
+ax = fig_night.add_subplot(1, 1, 1)
+ax.scatter(sd[:,0], sd[:,1], alpha=0.08, lw=0.1, s=10, color='0.8')
+ax.contour(X, Y, Z_night, levels=cs.levels)
+ax.set_xlim(-74.05, -73.75)
+ax.set_ylim(40.55, 40.9)
+fig_night.savefig('./include/gmm_cat_night.png', bbox_inches='tight', dpi=400)
